85facc445427b622922da28b17c15d6092a054b5
Benjamin Lupton <b@lupton.cc>
2016-03-20 09:25:19 +0800

    updated base files

--- esnextguardian.js
+++
@@ -1,8 +0,0 @@
-// 2015 December 8
-// https://github.com/bevry/esnextguardian
-'use strict'
-module.exports = require('esnextguardian')(
-    require('path').join(__dirname, 'esnext', 'lib', 'typechecker.js'),
-    require('path').join(__dirname, 'es5', 'lib', 'typechecker.js'),
-    require
-)
